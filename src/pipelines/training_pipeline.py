ffee7c292713a31060f8ca6b7e90fb64350a588b
SAI KIRAN PATNANA <saikiranpatnana@SAIs-MacBook-Pro.local>
2023-06-21 13:31:26 +0530

    Data-Transformation

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -4,8 +4,12 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 from src.components.data_ingestion import DataIngestion
-
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data_path,test_data_path = obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
+    data_ingestion = DataIngestion()
+    data_transformation = DataTransformation()
+    train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+    train_array,test_array,preprocessing_obj_file_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    # print(train_data_path,test_data_path)
+    print(preprocessing_obj_file_path)
